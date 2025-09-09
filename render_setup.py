76a7fddac143c30c5867a2eb761eeb9302f1218d
Disha Jain <jain.disha104@gmail.com>
2025-09-09 22:06:08 +0530

    Create render_setup.py

---
+++ render_setup.py
@@ -0,0 +1,7 @@
+# render_setup.py
+import os
+from database import engine, Base
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
+print("Database tables created successfully for production!")
